554435c4035770f841df349f59dfd4085ba62d9b
Richard <richard.yang.tong@gmail.com>
2021-04-24 21:01:37 -0400

    Added a generic function.

--- functionGenerics.ts
+++ functionGenerics.ts
@@ -1,11 +1,13 @@
-const printStrings = (arr: string[]): void => {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
+// const printStrings = (arr: string[]): void => {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//   }
+// }
 
-const printNumbers = (arr: number[]): void => {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
+// const printNumbers = (arr: number[]): void => {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//   }
+// }
+
+const printAnything = <T>(arr: T[]) => {}
