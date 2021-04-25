7587c15817ec8190fbefbcbc4518fefd517256d1
Richard <richard.yang.tong@gmail.com>
2021-04-24 20:48:16 -0400

    Added a generic class.

--- generics.ts
+++ generics.ts
@@ -1,15 +1,17 @@
-class ArrayOfNumbers {
-  constructor(public collection: number[]) {}
+// class ArrayOfNumbers {
+//   constructor(public collection: number[]) {}
 
-  get(index: number): number {
-    return this.collection[index];
-  }
-}
+//   get(index: number): number {
+//     return this.collection[index];
+//   }
+// }
 
-class ArrayOfStrings {
-  constructor(public collection: string[]) {}
+// class ArrayOfStrings {
+//   constructor(public collection: string[]) {}
 
-  get(index: number): string {
-    return this.collection[index];
-  }
-}
+//   get(index: number): string {
+//     return this.collection[index];
+//   }
+// }
+
+class ArrayOfAnything<T> {}
