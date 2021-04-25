5bf6130896fcdf7b1dfd86155e75b2107886e1b7
Richard <richard.yang.tong@gmail.com>
2021-04-24 20:59:57 -0400

    Completed printString().

--- functionGenerics.ts
+++ functionGenerics.ts
@@ -1,3 +1,7 @@
-const printStrings = () => {}
+const printStrings = (arr: string[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
 
 const printNumbers = () => {}
